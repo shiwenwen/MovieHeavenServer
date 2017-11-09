fee27afcd2e4e10e92349c28573e2a2c6b12fbaf
石文文 <shiwenwendevelop@163.com>
2017-11-09 17:23:44 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2017-11-09 09:17:30 +0000
+import PackageDescription
+let package = Package(
+	name: "MovieHeaven",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+	]
+)
